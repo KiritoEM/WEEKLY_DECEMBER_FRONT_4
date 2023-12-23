8c2c33396002f942995d9617d25448acca2682ee
kiritoEM <johankirito64@gmail.com>
2023-12-23 14:27:26 +0100

    add header to testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,11 +1,11 @@
-const Testimonials = () : JSX.Element => {
-    return (
-        <section id="testimonials">
-            <div className="title">
-                 <h2></h2>
-            </div>
-        </section>
-    );
+const Testimonials = (): JSX.Element => {
+  return (
+    <section id="testimonials">
+      <div className="title">
+        <h2>Testimonials</h2>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;

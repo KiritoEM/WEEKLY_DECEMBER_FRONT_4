3b3143582e7a23640794b89f1f7c550e7df63433
kiritoEM <johankirito64@gmail.com>
2023-12-23 15:48:36 +0100

    add responsive to CTA

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -3,7 +3,7 @@ const CTA = (): JSX.Element => {
     <section id="CTA">
       <div id="CTA__container">
         <div className="row">
-          <div className="col-6">
+          <div className="col-md-6">
             <div className="section-header">
               <div className="title">
                 <h2>OpenType feature and Variable fonts</h2>
@@ -13,10 +13,15 @@ const CTA = (): JSX.Element => {
               </div>
             </div>
           </div>
-          <div className="col-6">
-             <div className="section-cover">
-                  <img src="/CTA-img.png" alt="" />
-             </div>
+          <div className="col-6 d-none d-md-flex">
+            <div className="section-cover">
+              <img src="/CTA-img.png" alt="" />
+            </div>
+          </div>
+          <div className="col-md-6 mt-5">
+            <div className="section-cover d-md-none">
+              <img src="/CTA-2.png" alt="" />
+            </div>
           </div>
         </div>
       </div>

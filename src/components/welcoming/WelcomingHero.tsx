c62ac75f5cb9400fbcfbb7e50ba14b32c7ef8962
kiritoEM <johankirito64@gmail.com>
2023-12-23 08:44:56 +0100

    add hero section

--- src/components/welcoming/WelcomingHero.tsx
+++ src/components/welcoming/WelcomingHero.tsx
@@ -1,11 +1,25 @@
-const WelcomingHero = () : JSX.Element => {
-    return (
-        <section className="welcoming-hero">
-            <div className="title"></div>
-            <div className="text"></div>
-            <div className="buttons"></div>
-        </section>
-    );
+const WelcomingHero = (): JSX.Element => {
+  return (
+    <section className="welcoming-hero">
+      <div className="title">
+        <h1>Work at the speed of thought</h1>
+      </div>
+      <div className="text">
+        <p>
+          Most calendars are designed for teams. Slate is designed for
+          freelancers who want a simple way to plan their schedule.
+        </p>
+      </div>
+      <div className="buttons">
+        <div className="btn-1">
+          <button className="btn">Try For Free</button>
+        </div>
+        <div className="btn-2">
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default WelcomingHero;

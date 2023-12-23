599070c712406e68ac5e0716f848dd6aed2f0f1b
kiritoEM <johankirito64@gmail.com>
2023-12-23 14:42:03 +0100

    add testimonials-card

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,8 +1,17 @@
+import TestimonialsCard from "./testimonials/TestimonialsCard";
+
 const Testimonials = (): JSX.Element => {
   return (
     <section id="testimonials">
       <div className="title">
         <h2>Testimonials</h2>
+      </div>
+      <div className="testimonials-content">
+        <div className="row">
+          <div className="col-6">
+            <TestimonialsCard />
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/testimonials/TestimonialsCard.tsx
@@ -0,0 +1,25 @@
+const TestimonialsCard = (): JSX.Element => {
+  return (
+    <div className="testimonials-card">
+      <div className="client">
+        <div className="img">
+          <img src="/avatar-1.png" alt="" />
+        </div>
+        <div className="info">
+          <p>Claire Bell</p>
+          <h6>Designer</h6>
+        </div>
+      </div>
+      <div className="text">
+        <p>
+          Slate helps you see how many more days you need to work to reach your
+          financial goal for the month and year. Slate helps you see how many
+          more days you need to work to reach your financial goal for the month
+          and year.your financial goal for the month and year.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsCard;

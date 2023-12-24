07a56bb760f1329df76982ae8841b976369e137e
kiritoEM <johankirito64@gmail.com>
2023-12-24 05:31:46 +0100

    add react awesome reveal

--- src/components/welcoming/WelcomingHero.tsx
+++ src/components/welcoming/WelcomingHero.tsx
@@ -1,23 +1,43 @@
+import Typewriter from "typewriter-effect";
+import { Fade } from "react-awesome-reveal";
+
 const WelcomingHero = (): JSX.Element => {
   return (
     <section className="welcoming-hero">
       <div className="title">
-        <h1>Work at the speed of thought</h1>
+        <h1>
+          <Typewriter
+            options={{
+              loop: true,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Work at the speed of thought")
+                .pauseFor(1000)
+                .deleteAll()
+                .start();
+            }}
+          />
+        </h1>
       </div>
-      <div className="text">
-        <p>
-          Most calendars are designed for teams. Slate is designed for
-          freelancers who want a simple way to plan their schedule.
-        </p>
-      </div>
-      <div className="buttons">
-        <div className="btn-1">
-          <button className="btn">Try For Free</button>
+      <Fade direction="down">
+        <div className="text">
+          <p>
+            Most calendars are designed for teams. Slate is designed for
+            freelancers who want a simple way to plan their schedule.
+          </p>
         </div>
-        <div className="btn-2">
-          <button className="btn">Learn More</button>
+      </Fade>
+      <Fade cascade direction="up">
+        <div className="buttons">
+          <div className="btn-1">
+            <button className="btn">Try For Free</button>
+          </div>
+          <div className="btn-2">
+            <button className="btn">Learn More</button>
+          </div>
         </div>
-      </div>
+      </Fade>
     </section>
   );
 };
